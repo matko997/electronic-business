f2f9463e808a5c4eb443db9e96d57bf7fda90dee
matko <matko.dugandzic@fsre.sum.ba>
2024-01-31 21:32:58 +0100

    Project setup

---
+++ src/main/java/com/electronicbusiness/bidmaster/BidMasterApplication.java
@@ -0,0 +1,13 @@
+package com.electronicbusiness.bidmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/electronicbusiness/bidmaster/BidMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electronicbusiness.bidmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
